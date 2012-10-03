5cc060f55138dc74ef52f60134628fa308866251
Kingdon Barrett <kingdon@tuesdaystudios.com>
2012-10-02 21:43:07 -0400

    ... end of document syntax
    for network streams, ending a document without starting another on a
    stream channel is accomplished by typing "..." on a line by itself.

--- mustache.rb
+++ mustache.rb
@@ -10,7 +10,6 @@ CSV.foreach('./mint-export.csv') do |row|
 - Amount:{{Amount}}\tTransactionType:{{TransactionType}}\tCategory:{{Category}}
 - AccountName:{{AccountName}}\tLabels:{{Labels}}
 - Notes:{{Notes}}
-...
 ", :Date => row[0], :Description => row[1], :OriginalDescription => row[2],
 :Amount => row[3], :TransactionType => row[4], :Category => row[5],
 :AccountName => row[6], :Labels => row[7], :Notes => row[8])

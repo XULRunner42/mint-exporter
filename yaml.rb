b86eb31456bdfa353212578cdabe3329652ab729
Kingdon Barrett <kingdon@tuesdaystudios.com>
2012-10-02 22:24:31 -0400

    print the doc more actively
    you can do a different thing on/for every line

--- yaml.rb
+++ yaml.rb
@@ -5,5 +5,30 @@ require 'ap'
 
 log = File.open( "mustache.out" )
 yp = YAML::load_documents( log ) { |doc|
-  ap doc
+  first, *rest = doc[0].split("\t")
+  puts "#{first}"
+  puts rest
+  ap doc[1]
+  ap doc[2]
+  print "Account/Labels: "
+  ap doc[3]
+  if("String"==doc[3].class.to_s) then
+    puts "String:#{doc[3]}"
+  elsif("Hash"==doc[3].class.to_s) then
+    doc[3].each do |a|
+      if("Array"==a.class.to_s) then
+	puts "value:#{a}"
+	print "Label: "
+	ap a[0]
+	print "Value: "
+	ap a[1]
+      else
+	puts "Exception!"
+      end
+    end
+  else
+    puts "Exception!"
+  end
+  print "Notes: "
+  ap doc[4]["notes"]
 }
